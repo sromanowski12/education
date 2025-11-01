f7dbe16333c2cdcb36a0fa48d130be66b594aced
Seth Romanowski <sromanowski12@gmail.com>
2025-11-01 14:54:03 -0400

    HTTP Request

---
+++ templates/api/github-app.bash
@@ -0,0 +1,5 @@
+curl --request GET \
+--url "https://api.github.com/meta" \
+--header "Accept: application/vnd.github+json" \
+--header "Authorization: Bearer INSTALLATION_ACCESS_TOKEN" \
+--header "X-GitHub-Api-Version: 2022-11-28"
